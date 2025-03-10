e152c268fb41a3ea2ccfeadd0f898e0613cc9700
MaTeS72 <martincarska@gmail.com>
2025-03-10 15:53:47 +0100

    posts service

--- lib/api/tumblr_api.dart
+++ lib/api/tumblr_api.dart
@@ -1,3 +1,4 @@
+import 'package:tumblr_api/api/posts/posts_service.dart';
 import 'package:tumblr_api/api/user_v2/user_v2_service.dart';
 
 abstract class TumblrApi {
@@ -5,18 +6,19 @@ abstract class TumblrApi {
 
   UserService get user;
 
-  // BlogService get blog;
+  PostsService get posts;
 }
 
 class _TumblrApi implements TumblrApi {
   final String accessToken;
 
   _TumblrApi(this.accessToken)
-      : user = UserService(accessToken: accessToken);
+      : user = UserService(accessToken: accessToken),
+        posts = PostsService(accessToken: accessToken);
 
   @override
   final UserService user;
 
-  // @override
-  // final ThreadsMediaService media;
+  @override
+  final PostsService posts;
 }
