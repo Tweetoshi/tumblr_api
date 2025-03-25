11eb4ea5e0bd450f824e16c7510d4b0fe198a342
MaTeS72 <martincarska@gmail.com>
2025-03-25 13:07:20 +0100

    example todo

---
+++ example/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // TODO: add example code here
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
